564f21f73e9d681ca81300a676a0948a980e0492
Alvin Faiz <softkopi3258@gmail.com>
2021-02-01 16:37:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const date3 = new Date('12/5/2021');
 const diffTime = Math.abs(date2 - date1);
 const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
 const diffTime1 = Math.abs(date2 - date1);
-const diffDays1 = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+const diffDays1 = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+console.log(date1);
 $('#quote').text(`Ramadhan ${diffDays}  Hari lagi. Lebaran ${diffDays1} hari lagi.`);
